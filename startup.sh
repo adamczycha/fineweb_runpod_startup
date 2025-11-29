d39ccf329fe7c0e7b072bdf6628b42b101c135a7
Marcel <marcel.kowalik.rekrutacja@gmail.com>
2025-11-29 22:24:20 +0100

    auto export

--- startup.sh
+++ startup.sh
@@ -3,7 +3,10 @@
 git clone --branch runpod_classify --single-branch --depth 1 https://adamczyha:${apikey_github}@github.com/adamczycha/fineweb2_bis.git 
 
 cd fineweb2_bis
+
+set -a
 source ./classification_config
+set +a
 
 cp -a $MODEL_CHECKPOINT .
 
