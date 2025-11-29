3cd0fe3cfe450a75899504648f45671d397ed71d
Marcel Kowalik <marcel.kowalik.general@gmail.com>
2025-11-29 20:04:23 +0100

    Add startup script for cloning and running classification

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+git clone --branch rupod_classify_init https://adamczyha:${apikey_github}@github.com/adamczycha/fineweb2_bis.git 
+
+cd fineweb2_bis
+source ./classification_config
+
+cp -a $MODEL_CHECKPOINT .
+
+python -m pip install -r requirements.txt
+
+python classify.py
+if [-n "$RUNPOD_POD_ID"]; then 
+    runpodctl remove pod $RUNPOD_POD_ID
+fi
